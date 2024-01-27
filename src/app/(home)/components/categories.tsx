6a6b1649ee279d62e4b65d102d84ea02f2c2a2b8
clemilson chaves <cle_milson_silva@hotmail.com>
2024-01-26 22:29:35 -0300

    ajuste no bg

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -1,52 +1,48 @@
 import React from "react";
-import { badgeVariants } from "@/components/ui/badge"
-
-
 import { Car } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 const Categories = () => {
     return ( 
-      <div className="grid grid-cols-2 m-auto gap-x-4 gap-y-2   w-3/4 bg-slate-400 p-4 ">
+      <div className="grid grid-cols-2 m-auto gap-x-4 gap-y-2   w-3/4  p-4 ">
         
-         <Button variant={"outline"} className="flex justify-center items-center m-auto w-3/4 gap-1 bg-orange-500">
+         <Button variant="outline" className="flex justify-center items-center m-auto w-3/4 gap-1 bg-orange-500">
             <Link className="flex justify-center items-center gap-2"  href="#" >
               <Car className="size-8" />
                 Carros
             </Link>
          </Button>
           
-         <Button variant={"outline"} className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
+         <Button variant="outline" className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
             <Link className="flex justify-center items-center gap-2"  href="#" >
               <Car className="size-8" />
                 Carros
             </Link>
          </Button>
           
-         <Button variant={"outline"} className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
+         <Button variant="outline" className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
             <Link className="flex justify-center items-center gap-2"  href="#" >
               <Car className="size-8" />
                 Carros
             </Link>
          </Button>
 
-         <Button variant={"outline"} className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
+         <Button variant="outline" className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
             <Link className="flex justify-center items-center gap-2"  href="#" >
               <Car className="size-8" />
                 Carros
             </Link>
          </Button>
 
-         <Button variant={"outline"} className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
+         <Button variant="outline" className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
             <Link className="flex justify-center items-center gap-2"  href="#" >
               <Car className="size-8" />
                 Carros
             </Link>
          </Button>
 
-         <Button variant={"outline"} className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
+         <Button variant="outline" className="flex justify-center items-center m-auto w-3/4 gap-1  bg-orange-500">
             <Link className="flex justify-center items-center gap-2"  href="#" >
               <Car className="size-8" />
                 Carros
